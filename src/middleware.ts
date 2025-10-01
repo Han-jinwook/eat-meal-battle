a0e6dcd70240ae81d93aecd42fc821154b8734ef
Han-jinwook <chiu3@naver.com>
2025-10-01 10:41:22 +0900

    갤럭시 - 카카오 로그인 실패4 -최소코딩

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,6 +7,7 @@ export async function middleware(request: NextRequest) {
   const isProtectedPath = protectedPaths.some(path => nextUrl.pathname.startsWith(path));
 
   const supabase = createClient();
+    await supabase.auth.getSession(); // 세션 강제 동기화
   const { data: { user }, error } = await supabase.auth.getUser();
 
   if (isProtectedPath && (!user || error)) {
