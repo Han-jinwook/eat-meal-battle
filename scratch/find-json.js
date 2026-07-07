3de3609f7a295e4ce30c37546851b2c92f580d8c
Han-jinwook <chiu3@naver.com>
2026-07-07 11:20:29 +0900

    chore: remove temporary test scripts

--- scratch/find-json.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('scratch/yaksu.html', 'utf8');
-
-const target = '우리집떡볶이';
-let index = html.indexOf(target);
-if (index !== -1) {
-  console.log('Surrounding JSON for ' + target + ':');
-  console.log(html.substring(index - 300, index + 300));
-}
--- test-meta.js
+++
@@ -1,5 +0,0 @@
-const url = "https://naver.me/5ss0Zdnd";
-fetch(`https://whateat.sundreamer.app/api/naver-place-meta?url=${encodeURIComponent(url)}`)
-  .then(res => res.json())
-  .then(data => console.log(data))
-  .catch(err => console.error(err));
